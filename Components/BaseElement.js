11a5920dc78795a641ab71addd3c66fe68201719
Alan Mocaer <alanmoc@hotmail.fr>
2024-03-20 23:36:29 +0100

    Création du TextField. Reste à implémenter les listeners et les actions.

--- Components/BaseElement.js
+++ Components/BaseElement.js
@@ -1,33 +1,55 @@
 export class BaseElement extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: "open" });
-        this.constructor.attrs.forEach((attr) => {
-            Object.defineProperty(this.constructor.prototype, attr, {
-                get() {
-                    return this.getAttribute(attr);
-                },
-                set(newValue) {
-                    this.setAttribute(attr, newValue);
-                },
-            });
-        });
-    }
-    static get observedAttributes() {
-        return this.attrs;
-    }
-    connectedCallback() {
-        this.update();
-    }
-    attributeChangedCallback(name, oldValue, newValue) {
-        this.update();
-    }
-    update() {
-        this.shadowRoot.innerHTML = this.style()
-            ? `<style>${this.style()}</style>${this.template()}`
-            : this.template();
-    }
-    style() {
-        return null;
-    }
+	constructor() {
+		super();
+		this.rendered = false;
+		this.attachShadow({ mode: "open" });
+		this.constructor.attrs.forEach((attr) => {
+			Object.defineProperty(this.constructor.prototype, attr, {
+				get() {
+					return this.getAttribute(attr);
+				},
+				set(newValue) {
+					this.setAttribute(attr, newValue);
+				},
+			});
+		});
+	}
+	static get observedAttributes() {
+		return this.attrs;
+	}
+	connectedCallback() {
+		this.update();
+	}
+	attributeChangedCallback(name, oldValue, newValue) {
+		if (this.rendered) this.update();
+	}
+	update() {
+		this.shadowRoot.innerHTML = this.style() ? `<style>${this.style()}</style>${this.template()}` : this.template();
+		this.rendered = true;
+	}
+	style() {
+		return `
+            @import url('../public/themes/light.css');
+            @import url('../public/google_icons.css');
+        `;
+	}
+	template() {
+		return null;
+	}
+	getDetail() {
+		let detail = {};
+		for (let attr of this.constructor.attrs) {
+			detail[attr] = this[attr];
+		}
+		return detail;
+	}
+	dispatch(elt, eventName) {
+		elt.addEventListener(eventName, (event) => {
+			let customEvt = new CustomEvent(eventName, {
+				...event,
+				detail: this.getDetail(),
+			});
+			this.dispatchEvent(customEvt);
+		});
+	}
 }
--- Components/NumberField.js
+++ Components/NumberField.js
@@ -1,10 +1,10 @@
 import { TextField } from "./TextField.js";
 
 export class NumberField extends TextField {
-    static attrs = ["value", "placeholder", "min", "max", "type", "bordercolor"];
-    connectedCallback() {
-        super.connectedCallback();
-        this.type = "number";
-        this.placeholder = "0";
-    }
+	static attrs = TextField.attrs;
+	attrs = [...TextField.attrs, "decimal", "integer", "min", "max"];
+	connectedCallback() {
+		super.connectedCallback();
+		this.type = "number";
+	}
 }
